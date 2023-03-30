2b237cdf6407ec6cc0014217b7598ddb2ed66a20
Alberto-Zapardiel-Fernandez <alberto.zapardiel.fernandez@gmail.com>
2023-03-30 13:07:45 +0200

    Medio juego creado

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,71 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './App.css'
 import Ahorcado from './Components/Ahorcado.jsx'
-
-const listaPalabras = ['pepino', 'cabeza', 'fregona', 'fecha', 'alberto']
-
-const numeroAleatorio = () => {
-  return Math.floor(Math.random() * listaPalabras.length)
-}
+import LetrasIntroducidas from './Components/LetrasIntroducidas.jsx'
+import { revisaTurno, palabra, limpiar } from './functions.js'
 
 const App = () => {
-  const palabra = listaPalabras[numeroAleatorio()]
-  console.log(palabra)
+  const [turno, setTurno] = useState(0)
+  const [letraIntroducida, setLetraIntroducida] = useState('')
+  const [letrasIntroducidas, setLetrasIntroducidas] = useState([])
+
+  const comprobarEnPalabra = letra => {
+    if (palabra.includes(letra)) {
+      //TODO: Ver que salgan todas las letras bien en el string o transformarlo en array y buscar sus posiciones para luego revelarlas
+      console.log('La palabra: ' + palabra + ' tiene la ' + letra)
+    } else {
+      //TODO: Poner mensaje de que la palabra no contiene la letra
+      console.log('La palabra no tiene la ' + letraIntroducida)
+      setTurno(turno + 1)
+      return
+    }
+  }
+
+  const comprobarLetra = letraIntroducida => {
+    const letra = e => e == letraIntroducida
+    if (!letrasIntroducidas.some(letra)) {
+      setLetrasIntroducidas([...letrasIntroducidas, letraIntroducida])
+      comprobarEnPalabra(letraIntroducida)
+    } else {
+      //TODO: Poner mensaje de que ya está metida la letra
+      console.log('Ya introducida la ' + letraIntroducida)
+      setTurno(turno + 1)
+      return
+    }
+  }
+  const ponerLetra = letra => setLetraIntroducida(letra.target.value.toUpperCase())
 
   return (
     <div className='container'>
       <h1>Ahorcado</h1>
       <div className='tablero'>
         <div className='imagen'>
-          Texto
-          <Ahorcado></Ahorcado>
+          {/* //TODO: Poner la imagen y fin del juego */}
+          <Ahorcado imagen={revisaTurno(turno)}></Ahorcado>
+          <button onClick={() => setTurno(turno + 1)}>Pasa turno</button>
         </div>
         <div className='juego'>
-          <div className='letras'>letras</div>
+          {letrasIntroducidas.length > 0 ? <LetrasIntroducidas>{letrasIntroducidas}</LetrasIntroducidas> : <p></p>}
+          <div>
+            {/* //TODO: Crear componente con input y boton */}
+            <input
+              placeholder='Introduce solo una letra'
+              type='text'
+              name='letraIntroducida'
+              id='letraIntroducida'
+              minLength={1}
+              maxLength={1}
+              onChange={ponerLetra}
+              onFocus={limpiar}
+            />
+            <button
+              className='btn_separated'
+              onClick={() => comprobarLetra(letraIntroducida)}
+            >
+              Comprobar letra
+            </button>
+          </div>
+          {/* //TODO: Crear componente con las letras underlined pero ocultas */}
           <div className='textoResolucion'>{`Palabra: ${palabra.split('')}`}</div>
         </div>
       </div>
