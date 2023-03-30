9d34c8e9d2126a1b8a8a75449dfde0076bced1c1
Alberto-Zapardiel-Fernandez <alberto.zapardiel.fernandez@gmail.com>
2023-03-30 13:06:12 +0200

    Cambiadas las pruebas por imagenes

--- src/Components/Ahorcado.jsx
+++ src/Components/Ahorcado.jsx
@@ -1,18 +1,12 @@
 import React from 'react'
 
-const Ahorcado = () => {
+const Ahorcado = ({ imagen }) => {
   return (
-    <div className='ahorcado'>
-      <span></span>
-      <span>Cabeza</span>
-      <span></span>
-      <span>Brazo Izquierdo</span>
-      <span>Cuerpo</span>
-      <span>Brazo Derecho</span>
-      <span>Pierna Izquierda</span>
-      <span></span>
-      <span>Pierna Derecha</span>
-    </div>
+    <img
+      src={imagen}
+      alt='Ahorcado'
+      style={{ height: '70%' }}
+    />
   )
 }
 
