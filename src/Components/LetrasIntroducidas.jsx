e46da4e011d5ab8f3dfdf5faf99fd471b99ad0e0
Alberto-Zapardiel-Fernandez <alberto.zapardiel.fernandez@gmail.com>
2023-03-31 13:14:33 +0200

    Cambiada la forma de ver las letras introducidas

--- src/Components/LetrasIntroducidas.jsx
+++ src/Components/LetrasIntroducidas.jsx
@@ -3,13 +3,11 @@ import React from 'react'
 const LetrasIntroducidas = ({ children }) => {
   let letras = ''
   if (children.length > 0) {
-    letras = children.map(child => child + ',')
+    letras = children.map(child => child + '-')
   }
   return (
     <>
-      <h2>Lista de letras introducidas</h2>
-      <div className='letrasIntroducidas'>{letras}</div>
-      <div></div>
+      <div className='letrasIntroducidas'>{letras.join('').substring(0, letras.join('').length - 1)}</div>
     </>
   )
 }
