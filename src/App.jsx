356f11274cb88286516e5573251fdb5b77367e4d
Alberto-Zapardiel-Fernandez <alberto.zapardiel.fernandez@gmail.com>
2023-03-31 13:16:09 +0200

    Componente input y fin del juego

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,30 @@
 import React, { useEffect, useState } from 'react'
 import './App.css'
 import Ahorcado from './Components/Ahorcado.jsx'
+import Input from './Components/Input.jsx'
 import LetrasIntroducidas from './Components/LetrasIntroducidas.jsx'
 import Palabra from './Components/Palabra.jsx'
-import { revisaTurno, palabra, limpiar } from './functions.js'
+import { revisaTurno, palabra, revisaFinal } from './functions.js'
 
 const App = () => {
   let i = 0
   const [turno, setTurno] = useState(0)
+  const [mensaje, setMensaje] = useState('')
   const [letraIntroducida, setLetraIntroducida] = useState('')
   const [letrasIntroducidas, setLetrasIntroducidas] = useState([])
   const [winner, setWinner] = useState(0)
 
   useEffect(() => {
-    revisaFinal(winner)
-  }, [winner])
-
-  const revisaFinal = win => {
-    if (win === palabra.length) {
-      setTurno(12)
+    const final = revisaFinal(winner, setTurno)
+    if (final) {
+      setTimeout(() => {
+        alert('¡Ganaste!')
+        setTurno(0)
+        setLetraIntroducida('')
+        setLetrasIntroducidas([])
+      }, 500)
     }
-  }
+  }, [winner])
 
   const comprobarEnPalabra = letra => {
     if (palabra.includes(letra)) {
@@ -29,10 +33,22 @@ const App = () => {
           i += 1
         }
       })
+      setMensaje(`La palabra tiene ${i > 1 ? i + ' veces ' : i + ' vez '} la letra ${letraIntroducida} `)
+      setLetraIntroducida('')
     } else {
-      //TODO: Poner mensaje de que la palabra no contiene la letra
-      console.log('La palabra no tiene la ' + letraIntroducida)
-      setTurno(turno + 1)
+      setMensaje('La palabra no tiene la ' + letraIntroducida)
+      setLetraIntroducida('')
+      const newTurn = turno + 1
+      setTurno(newTurn)
+      if (newTurn === 11) {
+        setTimeout(() => {
+          alert('Perdiste!')
+          setTurno(0)
+          setLetraIntroducida('')
+          setLetrasIntroducidas([])
+          setMensaje('Perdiste!')
+        }, 500)
+      }
       return
     }
     setWinner(i + winner)
@@ -40,17 +56,20 @@ const App = () => {
 
   const comprobarLetra = letraIntroducida => {
     const letra = e => e == letraIntroducida
-    if (!letrasIntroducidas.some(letra)) {
-      setLetrasIntroducidas([...letrasIntroducidas, letraIntroducida])
-      comprobarEnPalabra(letraIntroducida)
+    document.getElementById('letraIntroducida').value = ''
+    if (letraIntroducida !== '') {
+      if (!letrasIntroducidas.some(letra)) {
+        setLetrasIntroducidas([...letrasIntroducidas, letraIntroducida])
+        comprobarEnPalabra(letraIntroducida)
+      } else {
+        setMensaje('Ya introducida la ' + letraIntroducida)
+        setTurno(turno + 1)
+        return
+      }
     } else {
-      //TODO: Poner mensaje de que ya está metida la letra
-      console.log('Ya introducida la ' + letraIntroducida)
-      setTurno(turno + 1)
-      return
+      setMensaje('Campo vacío')
     }
   }
-  const ponerLetra = letra => setLetraIntroducida(letra.target.value.toUpperCase())
 
   return (
     <div className='container'>
@@ -60,19 +79,10 @@ const App = () => {
           <Ahorcado imagen={revisaTurno(turno)}></Ahorcado>
         </div>
         <div className='juego'>
-          {letrasIntroducidas.length > 0 ? <LetrasIntroducidas>{letrasIntroducidas}</LetrasIntroducidas> : <p></p>}
+          <h2 className='mensaje'>{mensaje}</h2>
+          {letrasIntroducidas.length > 0 ? <LetrasIntroducidas>{letrasIntroducidas}</LetrasIntroducidas> : <h2></h2>}
           <div>
-            {/* //TODO: Crear componente con input y boton */}
-            <input
-              placeholder='Introduce solo una letra'
-              type='text'
-              name='letraIntroducida'
-              id='letraIntroducida'
-              minLength={1}
-              maxLength={1}
-              onChange={ponerLetra}
-              onFocus={limpiar}
-            />
+            <Input setLetraIntroducida={setLetraIntroducida}></Input>
             <button
               className='btn_separated'
               onClick={() => comprobarLetra(letraIntroducida)}
